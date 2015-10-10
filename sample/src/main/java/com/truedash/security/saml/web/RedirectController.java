769c59fb4a1866b4dac11f69cde97f880a22850f
Nagulapalle, Rajender <rnagulapalle@paypal.com>
2015-10-10 10:09:17 -0700

    committed mongo query changes..

--- sample/src/main/java/com/truedash/security/saml/web/RedirectController.java
+++ sample/src/main/java/com/truedash/security/saml/web/RedirectController.java
@@ -4,10 +4,13 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.opensaml.ws.wssecurity.Username;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,7 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.truedash.security.exception.NoSuchResourceFound;
 import com.truedash.security.exception.UnauthorizedException;
 import com.truedash.security.saml.util.EncryptDecryptUtil;
 
@@ -30,7 +34,7 @@ public class RedirectController {
 	 private MongoOperations mongoOperations;
 	 
 	@RequestMapping(value = "/truedash")
-    public String generateMetadata(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws UnauthorizedException, IllegalBlockSizeException {
+    public String generateMetadata(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws UnauthorizedException, IllegalBlockSizeException, NoSuchResourceFound {
 		
 		if(authentication != null) {
 			log.info("***** auth found  *** " +authentication.getName());
@@ -38,13 +42,23 @@ public class RedirectController {
 			log.info("********No auth found redirecting to saml login page***");
 			 return "redirect:/saml/login";
 		}
+		String userName = authentication.getName();
 		//check collection exists
 		if (mongoOperations.collectionExists("user")) {
 			//mongoOperations.dropCollection(Person.class);
 			log.info("*****USER COLLECTION FOUND IN THE DB******");
+			Query query = new Query();
+			query.addCriteria(Criteria.where("username").is("dariusz.zbik"));
+			if(mongoOperations.exists(query, "user")){
+				log.info("Authorized username {} found in the request...", userName);
+			}else{
+				
+				log.error("Unauthorized username {} found in the request...", userName);
+				throw new UnauthorizedException(userName + " user not authorized");
+			}
+		} else{
+			throw new NoSuchResourceFound("No collection found in db..");
 		}
-		
-		String userName = authentication.getName();
 		
 		MongoClient mongoClient = new MongoClient("172.17.2.214", 27017);
 		DB db = mongoClient.getDB("datawarehouse");
